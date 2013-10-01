ba6515a0a477478e39556e969826a1145d5cd3c4
mile <mile@Mile-Laptop>
2013-10-01 21:22:16 +0200

    Users login Bcrypt

--- protected/data/oddsleaders.sql
+++ protected/data/oddsleaders.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 30, 2013 at 10:45 PM
+-- Generation Time: Oct 01, 2013 at 09:21 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.3.10-1ubuntu3.8
 
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS `coefficient` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `game_id` int(10) unsigned NOT NULL,
   `house_id` int(10) unsigned NOT NULL,
-  `home_win` int(11) NOT NULL,
-  `guest_win` int(11) NOT NULL,
-  `draw` int(11) NOT NULL,
+  `home_win` float NOT NULL,
+  `guest_win` float NOT NULL,
+  `draw` float NOT NULL,
   PRIMARY KEY (`id`),
   KEY `game_id` (`game_id`),
   KEY `house_id` (`house_id`)
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS `game` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `home_id` int(10) unsigned NOT NULL,
   `guest_id` int(10) unsigned NOT NULL,
+  `start` datetime NOT NULL,
   `home_goals` int(10) unsigned NOT NULL,
   `guest_goals` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
@@ -99,6 +100,47 @@ CREATE TABLE IF NOT EXISTS `nickname` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `role` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `user_id`, `role`) VALUES
+(3, 6, 'administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `round`
+--
+
+CREATE TABLE IF NOT EXISTS `round` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `identificator` varchar(128) NOT NULL,
+  `date_from` datetime NOT NULL,
+  `date_to` datetime NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_modified` datetime NOT NULL,
+  `game_id` int(10) unsigned NOT NULL,
+  `house_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `house_id` (`house_id`),
+  KEY `game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `team`
 --
 
@@ -114,39 +156,26 @@ CREATE TABLE IF NOT EXISTS `team` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `email` varchar(64) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `active` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `date_created`, `active`) VALUES
-(2, 'admin', 'admin', 'Admin', '2028-10-20 11:00:00', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_roles`
---
-
-CREATE TABLE IF NOT EXISTS `user_roles` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `role` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `email`, `date_created`, `active`) VALUES
+(6, 'root', '$2a$12$wRm12jji8mQ9luaxSUSePuP4wm/oipFORRh51/ujvvpg4oYAPIeMC', 'daa', 'da@yahoo.com', '2013-10-01 18:58:04', 0);
 
 --
 -- Constraints for dumped tables
@@ -156,34 +185,41 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 -- Constraints for table `coefficient`
 --
 ALTER TABLE `coefficient`
-  ADD CONSTRAINT `coefficient_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `coefficient_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `coefficient_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `coefficient_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `game`
 --
 ALTER TABLE `game`
-  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`home_id`) REFERENCES `game` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `game` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `game_ibfk_4` FOREIGN KEY (`guest_id`) REFERENCES `team` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `game_ibfk_3` FOREIGN KEY (`home_id`) REFERENCES `team` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `nickname`
 --
 ALTER TABLE `nickname`
-  ADD CONSTRAINT `nickname_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `nickname_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `nickname_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `nickname_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `round`
+--
+ALTER TABLE `round`
+  ADD CONSTRAINT `round_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `round_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `team`
 --
 ALTER TABLE `team`
   ADD CONSTRAINT `team_ibfk_1` FOREIGN KEY (`league_id`) REFERENCES `league` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `user_roles`
---
-ALTER TABLE `user_roles`
-  ADD CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
