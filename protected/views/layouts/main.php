bed927660647c81331c587d5c9fcf89f4197d447
mile <mile@Mile-Laptop>
2013-11-03 21:49:36 +0100

    Colorbox login/signup

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -47,8 +47,6 @@
                                 array('label'=>'User', 'url'=>array('/user/index')),
                                 array('label'=>'Contact', 'url'=>array('/site/contact')),
                                 array('label'=>'Admin', 'url'=>array('/admin/admin')),
-                                array('label'=>'Register', 'url'=>array('/user/register'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
