1dda1e4ff2fdfe65d20e1fbdcf5b619fe6140f26
tiger_s <slavco.kuzeski@gmail.coom>
2013-12-05 00:43:06 +0100

    Bug fix and best tipsters design

--- protected/views/stack/getmatches.php
+++ protected/views/stack/getmatches.php
@@ -11,10 +11,7 @@
             </div>
         </li>
         <?php
-
-        $cookie = explode('|', $_COOKIE['myBets']);
-
-        if (isset($_COOKIE['myBets'])) {
+       if (isset($_COOKIE['myBets']) && $_COOKIE['myBets']!='') {
            $cookie = explode('|', $_COOKIE['myBets']);
             
            for ($i = 0; $i < count($cookie) - 1; $i++) {
@@ -23,20 +20,23 @@
                 $tipped[$exp[0]][$exp[4]][$exp[1]] = 'tipped';
             }
         }
+        foreach ($matches as $value) {
 
-        foreach ($matches as $key => $value) {
+            
             $odds = json_decode($value['data']);
 
             $match_odds = '';
             if ($odds) {
                 foreach ($odds->match as $key => $match) {
-                    if ($key != 'label')
-                        if(isset($tipped[$value['code']]))
-                            print_r($tipped[$value['code']]);
-                        //$tip = ucfirst($tip);
-                        //(isset($tipped[$value['code']][$key][$tip])) ? $tipe = 'tipped' : $tipe = '';
-                        
-                        $match_odds .= '<div class="tip"><a class="clickable" id="liga" rel="' . $value['code'] . '" data-type="'.ucfirst($key).'">' . $match . '</a></div>';
+                    $type = strtolower($match);
+
+                    if (isset($key) AND $key != 'label')
+                    {
+                        $key = (string)ucfirst($key);
+                        (isset($tipped[$value['code']]['match'][$key])) ? $tiped = 'tipped' : $tiped = '';
+
+                        $match_odds .= '<div class="tip"><span class="gameTypeBet">match</span><a class="clickable '.$tiped.'" id="liga" rel="' . $value['code'] . '" data-type="'.$key.'">' . $match . '</a></div>';
+                    }
                 }
             }
 
@@ -48,9 +48,15 @@
                     if ($key != 'match') {
 
                         foreach ($more_odds as $tip => $m_odds) {
+                            $m_odds = strtolower($m_odds);
+
                             if ($tip != 'label') {
+                                $tip = ucfirst($tip);
+                                    (isset($tipped[$value['code']][$key][$tip])) ? $tipe = 'tipped' : $tipe = '';
+
+                                
                                 if (!empty($m_odds))
-                                    $odd .= '<div class="tip"><a class="clickable" rel="' . $value['code'] . '"><b class="gameType">' . ucfirst($tip) . '</b> <span class="gameQuote">' . $m_odds . '</span><span class="gameTypeBet">'.$key.'</span></a></div>';
+                                    $odd .= '<div class="tip"><span class="gameTypeBet">'.$key.'</span><a class="clickable '.$tipe.'" rel="' . $value['code'] . '"><b class="gameType">' . ucfirst($tip) . '</b> <span class="gameQuote">' . $m_odds . '</span><span class="gameTypeBet">'.$key.'</span></a></div>';
                             }
                         }
 
@@ -62,10 +68,8 @@
                     }
                 }
             }
-
             $disable = '';
-                    
-            if (isset($_COOKIE['myBets'])) {
+            if (isset($_COOKIE['myBets']) && $_COOKIE['myBets']!='') {
                 foreach ($cookie as $key => $cook) {
                     $exp = explode('=', $cook);
                     if($exp[0] === $value['code'])
@@ -73,7 +77,7 @@
                 }
             }
 
-            echo '<li class="' . $value['code'] .' '. $disable .'">
+            echo '<li class="' . $value['code'] . " ".$disable.'">
                         <div id="time">' . date("d-m H:i", strtotime($value['start'])) . '</div>
                         <div id="teams"><a>' . $value['opponent'] . '</a></div>
                         <div class="tips">
@@ -85,6 +89,6 @@
                         </ul>
                     </li>';
         }
-        ?>
+?>
     </ul>
 </div>
--- protected/views/user/profile.php
+++ protected/views/user/profile.php
@@ -3,9 +3,9 @@
 	<div id="user_box">
 		<form method="POST" action="#" class="form">
 			<img src="<?=$model[0]['image'];?>"/>
-			<label>Name:<br><input type='text' value="<?=$model[0]['name'];?>"/></label>
-			<label>Email:<br><input type='text' value="<?=$model[0]['email'];?>"/></label>
-			<label>Member since: <?=date('d M Y', strtotime($model[0]['date_created']));?></label>
+			<label>Name:<br><input type='text' value="<?=$model[1]['name'];?>"/></label>
+			<label>Email:<br><input type='text' value="<?=$model[1]['email'];?>"/></label>
+			<label>Member since: <?=date('d M Y', strtotime($model[1]['date_created']));?></label>
 			<label>Credits: <?=$model[0]['conto'];?> <a href="">Buy more</a></label>
 			<input type="submit" value="Change" class="button blue">
 		</form>
