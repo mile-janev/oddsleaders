75b676560efb3394653942055c79357092c15cc1
mignix <mignix@git.drupal.org>
2014-09-29 09:53:50 +0200

    Fixes

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -50,7 +50,14 @@ class LoginForm extends CFormModel
 		{
 			$this->_identity=new UserIdentity($this->username,$this->password);
 			if(!$this->_identity->authenticate())
-				$this->addError('password','Incorrect username or password.');
+                        {
+                            $this->addError('all_errors','Погрешна е-пошта или лозинка.');
+                        }
+                        else
+                        {
+                            $duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
+                            Yii::app()->user->login($this->_identity,$duration);
+                        }
 		}
 	}
 
